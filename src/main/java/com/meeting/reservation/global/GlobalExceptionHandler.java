b905e23da9bf7ea9acb8898fe59f70c1995715fc
apptie <appti@naver.com>
2025-11-11 01:00:36 +0900

    feat: 전역 예외 처리 추가

---
+++ src/main/java/com/meeting/reservation/global/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.meeting.reservation.global;
+
+import com.meeting.reservation.domain.reservation.Reservations;
+import com.meeting.reservation.domain.reservation.Reservations.InvalidReservationPasswordException;
+import com.meeting.reservation.domain.room.MeetingRooms.MeetingRoomNotFoundException;
+import com.meeting.reservation.persistence.InMemoryReservationRepository.ReservationNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest()
+                             .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Reservations.ReservationNotFoundException.class)
+    public ResponseEntity<String> handleReservationNotFoundException(Reservations.ReservationNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MeetingRoomNotFoundException.class)
+    public ResponseEntity<String> handleMeetingRoomNotFoundException(MeetingRoomNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ReservationNotFoundException.class)
+    public ResponseEntity<String> handleReservationNotFoundInRepositoryException(
+            ReservationNotFoundException ex
+    ) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidReservationPasswordException.class)
+    public ResponseEntity<String> handlerInvalidReservationPasswordException(InvalidReservationPasswordException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                             .body(ex.getMessage());
+    }
+}
