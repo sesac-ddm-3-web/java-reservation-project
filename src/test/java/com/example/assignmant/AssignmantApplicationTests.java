2a54e814650505a860e584169708aecf115a86a7
HyNS00 <hsk7953@gmail.com>
2025-11-10 17:58:30 +0900

    feat: first commit

---
+++ src/test/java/com/example/assignmant/AssignmantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
