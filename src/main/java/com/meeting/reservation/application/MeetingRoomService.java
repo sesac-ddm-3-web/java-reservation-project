b78f86450d6c2b9820e90663ea76d9d17e3782ea
apptie <appti@naver.com>
2025-11-11 00:51:11 +0900

    feat: 회의실 관련 애플리케이션 서비스 추가

---
+++ src/main/java/com/meeting/reservation/application/MeetingRoomService.java
@@ -0,0 +1,19 @@
+package com.meeting.reservation.application;
+
+import com.meeting.reservation.domain.room.MeetingRoom;
+import com.meeting.reservation.domain.room.repository.MeetingRoomRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MeetingRoomService {
+
+    private final MeetingRoomRepository meetingRoomRepository;
+
+    public List<MeetingRoom> findAll() {
+        return meetingRoomRepository.findAll()
+                                    .getMeetingRooms();
+    }
+}
