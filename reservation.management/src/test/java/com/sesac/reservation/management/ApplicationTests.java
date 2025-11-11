40de4a603675e3f144c7cf4998b09c14ede0e6cd
gyunnybot <idjkda4034@gmail.com>
2025-11-11 15:37:02 +0900

    feat: initial setting

---
+++ reservation.management/src/test/java/com/sesac/reservation/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.reservation.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
